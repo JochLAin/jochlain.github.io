c612e0397c4b13c90b4901266078211ef9309c6e
Jochlain <jocelyn.faihy@gmail.com>
2021-10-20 14:53:32 +0200

    fix translate

--- assets/utils/translate.ts
+++ assets/utils/translate.ts
@@ -1,7 +1,7 @@
 import IntlMessageFormat from 'intl-messageformat';
 import { TRANSLATION_CATALOGS } from "../constant";
 
-const translate = (key: string, parameters: { [key: string]: string|number|boolean } = {}, domain: string = 'messages', locale: string = 'fr'): string|(string|void)[]|void => {
+const translate = (key: string, parameters = {}, domain: string = 'messages', locale: string = 'fr'): string|(string|void)[]|void => {
     const message = TRANSLATION_CATALOGS?.[locale.toLowerCase()]?.[domain.toLowerCase()]?.[key];
     if (!message) return key;
 
