47a099c5170a9332edb8358a7f3c71cc9f757cd1
jocelyn <jocelyn@amtsolutions.fr>
2022-09-19 15:39:49 +0200

    retrieve navigator language preference

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,15 @@ import React, { useEffect } from 'react';
 
 export default function Index() {
     useEffect(() => {
-        let locale = localStorage.getItem('locale') || 'fr';
-        if (!['en', 'fr'].includes(locale)) locale = 'fr';
+        let locale = localStorage.getItem('locale');
+        if (!locale) {
+            try {
+                locale = navigator.language.split('-')[0];
+            } catch (e) {
+                locale = 'fr';
+            }
+        }
+        if (!['en', 'fr'].includes(locale)) locale = 'en';
         document.location.href = `/${locale}`;
     }, []);
 
