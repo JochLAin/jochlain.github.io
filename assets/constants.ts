64d1b5a31774d7349708c9345a0a4f2b766b16da
Jochlain <jocelyn.faihy@gmail.com>
2022-11-14 11:21:42 +0100

    fix experiences

--- assets/constants.ts
+++ assets/constants.ts
@@ -7,7 +7,7 @@ export interface APIExperienceInterface {
   readonly job?: string,
   readonly company?: string,
   readonly color: string,
-  readonly start?: string,
+  readonly start: string,
   readonly end?: string,
 }
 
@@ -19,23 +19,15 @@ export interface ExperienceInterface {
   readonly company?: string,
   readonly color: string,
   readonly start: Date,
-  readonly end: Date,
+  readonly end?: Date,
 }
 
 export const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-const MIN_DATE = new Date(2022, 10);
-const NOW = new Date();
-
 export const EXPERIENCES: ExperienceInterface[] = API.experiences.map((exp: APIExperienceInterface) => {
-  // let start = NOW < MIN_DATE ? MIN_DATE : NOW;
-  // let end = NOW < MIN_DATE ? MIN_DATE : NOW;
-  let start;
-  let end;
+  const [startYear, startMonth] = exp.start.split('-');
+  const start = new Date(Number(startYear), Number(startMonth) - 1);
 
-  if (exp.start) {
-    const [startYear, startMonth] = exp.start.split('-');
-    start = new Date(Number(startYear), Number(startMonth) - 1);
-  }
+  let end;
   if (exp.end) {
     const [endYear, endMonth] = exp.end.split('-');
     end = new Date(Number(endYear), Number(endMonth) - 1);
